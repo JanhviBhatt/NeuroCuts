fea182adbeed8011381ee4fefde60dc68bbe6a66
janhvibhatt <janhvibhatt112@gmail.com>
2025-04-23 12:28:29 +0530

    changes

---
+++ components/metadata.jsx
@@ -0,0 +1,5 @@
+export const metadata= {
+    title: "NeuroCuts - AI-Powered Short Video Generator",
+    description: "NeuroCuts is an AI-powered tool that helps you create short videos from long-form content. It uses advanced algorithms to analyze your content and generate engaging short videos that capture the essence of your message.",
+  };
+  
